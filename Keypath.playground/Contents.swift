bb5fe040b8353f3502afa6554856a1c8647c7ee2
Deniz Mersinlioğlu <mersinliogludeniz@gmail.com>
2019-01-16 15:56:41 +0300

    Key Path Done

--- Keypath.playground/Contents.swift
+++ Keypath.playground/Contents.swift
@@ -1,10 +1,13 @@
-import UIKit
+import Foundation
+// WWDC 2017: Whats New in Foundation
 
-class Kid{
-    var nickname: String = ""
-    var age: Double = 0.0
-    var bestFriend: Kid? = nil
-    var friends: [Kid] = []
+// (??) Research what is @objcMembers and dynamic means
+@objcMembers
+class Kid: NSObject{
+    dynamic var nickname: String = ""
+    dynamic var age: Double = 0.0
+    dynamic var bestFriend: Kid? = nil
+    dynamic var friends: [Kid] = []
     
     convenience init(nickname: String, age: Double){
         self.init()
@@ -20,13 +23,13 @@ let firstFriend = \Kid.friends[0] // -> Indirection through subscripts
 
 var ben = Kid(nickname: "Benji", age: 6)
 ben[keyPath: \Kid.nickname] = "Ben"
-let mia = Kid(nickname: "Mia", age: 4.5)
+let mia = Kid(nickname: "Mia", age: 6.5)
 
-let age = ben[keyPath: \Kid.age] // -> Research abouth subscripts in swift.
+let age = ben[keyPath: \Kid.age] // -> (??) Research about subscripts in swift.
 ben[keyPath: \Kid.nickname] = "Ben" // -> Mutate with keypath subscript.
 
 struct BirthdayParty{
-    let celebrant: Kid
+    var celebrant: Kid
     var theme: String
     var attending: [Kid]
 }
@@ -50,6 +53,63 @@ let birthdayBoysAgeAppended = partyPersonsAge(party: bensParty, participantPath:
 let firstAttendeesAges = partyPersonsAge(party: bensParty, participantPath: \.attending[0])
 
 //MARK: - Type Erased Key Paths
-let themes = ["Theme", "Attending", "Birthday Kid"]
-let partyPaths = [\BirthdayParty.theme, \BirthdayParty.attending, \BirthdayParty.celebrant] // -> [PartialKeyPath<BirthdayParty>]: Partially type-erased key paths
+let titles = ["Theme", "Attending", "Birthday Kid"]
+let partyPaths = [\BirthdayParty.theme, \BirthdayParty.attending, \BirthdayParty.celebrant]
+// -> [PartialKeyPath<BirthdayParty>]: Partially type-erased key paths
+// PartialKeyPaths know abouth their base type, but they can point to any valid key path for that particular base.
 
+for (title, partyPath) in zip(titles, partyPaths){
+    let partyValue = miasParty[keyPath: partyPath]
+    print("\(title) -> \(partyValue)\n")
+}
+
+extension BirthdayParty{
+    // Solve1: func blowCandles(ageKeyPath: ReferenceWritableKeyPath<BirthdayParty, Double>){} -> Using a ReferenceWritableKeyPath enables to change reference value.
+    // Solve2: mutating func blowCandles(ageKeyPath: WritableKeyPath<BirthdayParty, Double>){} -> not a right thing because we don't mutate the BirthdayPaty: (Struct: Value Type)
+    mutating func blowCandles(ageKeyPath: ReferenceWritableKeyPath<BirthdayParty, Double>){
+        let oldAge = self[keyPath: ageKeyPath]
+        self[keyPath: ageKeyPath] = floor(oldAge) + 1.0
+    }
+}
+
+bensParty.blowCandles(ageKeyPath: \.celebrant.age)
+print(bensParty[keyPath: \BirthdayParty.celebrant.age])
+
+// WritableKeyPath: Write directly into value-type base (inout/mutating)
+// ReferenceWritableKeyPath: Simply invokes a property setter on the reference type. -> Write into a reference-type base.
+// Inheritence Tree: AnyKeyPath -> PartialKeyPath<Base> -> KeyPath<Base, Property> -> WritableKeyPath<Base, Property> -> ReferenceWritableKeyPath<Base, Property>
+// AnyKeyPath -> Fully type erased key path: Multiple bases and multiple properties.
+
+
+//MARK: Key Value Observing
+// KVO: Cocoa's way of allowing objects to establish relationships to be notified about changes in their state.
+// Let consider when mias age changes.
+// Observe can be used in NSObject subclass. When you set the observation nil, the observation will toward down: Unregistering observation.
+// observation = (Observe using key path) {reaction closure}
+let observation = mia.observe(\.age) { observed, change in
+    // observed is same reference with mia, but it is given as parameter to prevent retain cycles.
+}
+
+@objcMembers class KindergartenController: NSObject{
+    dynamic var representedKid: Kid
+    var ageObservation: NSKeyValueObservation?
+    
+    init(representedKid: Kid){
+        self.representedKid = representedKid
+    }
+    
+    convenience init(kid: Kid) {
+        self.init(representedKid: kid)
+        ageObservation = observe(\.representedKid.age) { observed, change in
+            if observed.representedKid.age > 5 {
+                print("Happy birthday \(observed.representedKid.nickname)! Time for kindergarten")
+            }
+        }
+    }
+}
+
+let controller = KindergartenController(kid: mia)
+miasParty.blowCandles(ageKeyPath: \.celebrant.age)
+
+//MARK: - String Key Path
+let stringKeyPath = #keyPath(Kid.nickname)
