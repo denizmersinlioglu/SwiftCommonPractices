44cca9d68d5ff50b454c3316ffb21408a865196d
Deniz <mersinliogludeniz@gmail.com>
2019-10-22 02:10:17 +0300

    CoreGraphics init

---
+++ CoreGraphicsPractice/CoreGraphicsPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreGraphicsPractice
+//
+//  Created by Deniz Mersinlioğlu on 22.10.2019.
+//  Copyright © 2019 DevelopmentHouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
